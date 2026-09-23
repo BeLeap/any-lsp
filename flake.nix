a92f1ce8263c2fa0a26f36e050f6e8a1be63546a
BeLeap <beleap@beleap.dev>
2026-09-24 01:14:59 +0900

    Package VS Code extension with Nix

--- flake.nix
+++ flake.nix
@@ -44,10 +44,66 @@
               license = pkgs.lib.licenses.mit;
             };
           };
+
+          vscodeExtension = pkgs.stdenvNoCC.mkDerivation {
+            pname = "any-lsp-vscode";
+            version = cargoToml.package.version;
+            src = ./vscode-extension;
+
+            nativeBuildInputs = [ pkgs.zip ];
+
+            dontConfigure = true;
+            dontBuild = true;
+
+            installPhase = ''
+              vsixRoot="$TMPDIR/vsix"
+              mkdir -p "$vsixRoot/extension/bin" "$out"
+              cp -r . "$vsixRoot/extension"
+              cp ${package}/bin/any-lsp "$vsixRoot/extension/bin/any-lsp"
+              chmod +x "$vsixRoot/extension/bin/any-lsp"
+
+              cat > "$vsixRoot/[Content_Types].xml" <<'EOF'
+              <?xml version="1.0" encoding="utf-8"?>
+              <Types xmlns="http://schemas.openxmlformats.org/package/2006/content-types">
+                <Default Extension="json" ContentType="application/json" />
+                <Default Extension="js" ContentType="application/javascript" />
+                <Default Extension="md" ContentType="text/markdown" />
+                <Default Extension="any-lsp" ContentType="application/octet-stream" />
+                <Default Extension="vsixmanifest" ContentType="text/xml" />
+                <Override PartName="/extension.vsixmanifest" ContentType="text/xml" />
+              </Types>
+              EOF
+
+              cat > "$vsixRoot/extension.vsixmanifest" <<'EOF'
+              <?xml version="1.0" encoding="utf-8"?>
+              <PackageManifest Version="2.0.0" xmlns="http://schemas.microsoft.com/developer/vsx-schema/2011">
+                <Metadata>
+                  <Identity Language="en-US" Id="beleap.any-lsp" Version="${cargoToml.package.version}" Publisher="beleap" />
+                  <DisplayName>any-lsp</DisplayName>
+                  <Description xml:space="preserve">Language-agnostic definition and reference navigation for every file.</Description>
+                  <Categories>Programming Languages</Categories>
+                  <Properties>
+                    <Property Id="Microsoft.VisualStudio.Code.Engine" Value=">=1.85.0" />
+                  </Properties>
+                </Metadata>
+                <Installation>
+                  <InstallationTarget Version="[1.0,)" Id="Microsoft.VisualStudio.Code" />
+                </Installation>
+                <Dependencies />
+                <Assets>
+                  <Asset Type="Microsoft.VisualStudio.Code.Manifest" Path="extension/package.json" />
+                </Assets>
+              </PackageManifest>
+              EOF
+
+              (cd "$vsixRoot" && zip -qr "$out/any-lsp-vscode-${cargoToml.package.version}.vsix" .)
+            '';
+          };
         in
         {
           default = package;
           any-lsp = package;
+          vscode-extension = vscodeExtension;
         });
 
       apps = forAllSystems ({ pkgs }:
@@ -62,6 +118,7 @@
       checks = forAllSystems ({ pkgs }:
         {
           default = self.packages.${pkgs.stdenv.hostPlatform.system}.default;
+          vscode-extension = self.packages.${pkgs.stdenv.hostPlatform.system}.vscode-extension;
         });
 
       devShells = forAllSystems ({ pkgs }:
